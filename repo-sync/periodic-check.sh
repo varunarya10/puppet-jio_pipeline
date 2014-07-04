fe73caa24754667014d7999c32a384cee1107fdd
Rushi Agrawal <rushi.agr@gmail.com>
2014-07-04 16:50:38 +0530

    Add comments to periodic-check.sh

--- repo-sync/periodic-check.sh
+++ repo-sync/periodic-check.sh
@@ -1,18 +1,29 @@
 #! /bin/bash -eux
 
+# Periodic job to keep the local mirror updated. This script takes snapshot
+# of any arbitrary directory, if the update job actually says there ARE
+# updates to be downloaded for one particular repository and one particular
+# distribution. For example, you can say that for in.archive.ubuntu.com
+# repository, for Precise, if there are any updates, take a snapshot.
+# Currently, you cannot specify more than one repository, or distribution to
+# take snapshot against.
+#
+# This script writes status to a file, so that this script can be added to
+# a cron easily.
+
 MIRRORCONF=/etc/apt/mirror.list
+MIRRORSRC=/home/repo/repo/mirror
 DIST=precise-security
+RELEASE=/home/repo/repo/mirror/security.ubuntu.com/ubuntu/dists/$DIST/Release
+
 SNAPSRC=/home/repo/repo/skel
 SNAPDEST=/home/rushi/repo/snapshots
-MIRRORSRC=/home/repo/repo/mirror
-RELEASE=/home/repo/repo/mirror/security.ubuntu.com/ubuntu/dists/$DIST/Release
 
 STATUSFILE=/home/rushi/repo/sync-status.txt
 
 OLDMD5=`cat $RELEASE | grep main/binary-amd64/Packages$ | head -1 | cut -d' ' -f2`
 
-
-echo "\n---------------------------------------------" >> $STATUSFILE 
+echo "---------------------------------------------" >> $STATUSFILE 
 
 echo `sudo apt-mirror $MIRRORCONF` >> $STATUSFILE
 
