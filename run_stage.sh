33e190ee7530c6c17957acac4a6e5b51e2c88788
Yogeshwar Shenoy <ynshenoy@gmail.com>
2014-07-04 14:13:14 +0530

    Fix bug in stage script

--- run_stage.sh
+++ run_stage.sh
@@ -4,18 +4,18 @@ if [ -e STAGE-last-tested ]; then
     LASTTESTED=`cat STAGE-last-tested | cut -d'v' -f2`
 fi
 
-STAGE_LATEST=`cat STAGE-last-success | cut -d'v' -f2`
+NFT_LATEST=`cat NFT-last-success | cut -d'v' -f2`
 
-if [ $LASTTESTED -eq $STAGE_LATEST ]; then
+if [ $LASTTESTED -eq $NFT_LATEST ]; then
     echo nothing new to test
     exit 0
-elif [ $LASTTESTED -gt $STAGE_LATEST ]; then
-    echo LASTTESTED is greater than STAGE_LATEST!!
+elif [ $LASTTESTED -gt $NFT_LATEST ]; then
+    echo LASTTESTED is greater than NFT_LATEST!!
     exit 2
 fi
 
-echo Running automated staging tests for v$STAGE_LATEST
+echo Running automated staging tests for v$NFT_LATEST
 echo Tests successful!
 
-echo v$STAGE_LATEST > STAGE-last-tested
-echo v$STAGE_LATEST > STAGE-last-success
+echo v$NFT_LATEST > STAGE-last-tested
+echo v$NFT_LATEST > STAGE-last-success
