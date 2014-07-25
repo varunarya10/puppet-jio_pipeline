c46b0d74950d3d9b1c2e952bce7a2c17af906753
Yogeshwar Shenoy <ynshenoy@gmail.com>
2014-07-25 13:21:37 +0530

    Fix issues caused by Jenkins permalinks not behaving as promised in the docs

--- jenkins-jobs/run_acceptance.sh
+++ jenkins-jobs/run_acceptance.sh
@@ -4,25 +4,64 @@
 REPOSERVER="10.135.126.20:81"
 SNAPDIR=snapshots
 
+# helper to figure out version
+function get_target_version {
+    local SNAP_VER=0
+    if [ "$1" == "use_artifact" ]
+    then
+        SNAP_VER=`cat $2 | cut -d'v' -f2`
+    else
+        SNAP_VER=`echo "$1" | cut -d'v' -f2`
+    fi
+    echo "$SNAP_VER"
+}
+
+function get_last_tested {
+    local SNAP_VER=0
+    local VER1=0
+    local ARTIFACT1="lastsuccess/AT-last-tested"
+    local VER2=0
+    local ARTIFACT2="lastunsuccess/AT-last-tested"
+    if [ -e $ARTIFACT1 ]
+    then
+        VER1=`cat $ARTIFACT1 | cut -d'v' -f2` 
+    fi
+    if [ -e $ARTIFACT2 ]
+    then
+        VER2=`cat $ARTIFACT2 | cut -d'v' -f2`
+    fi
+    if [ $VER1 -gt $VER2 ]
+    then
+        SNAP_VER=$VER1
+    else
+        SNAP_VER=$VER2
+    fi
+    echo "$SNAP_VER" 
+}
+
 ## Deal with the proxy stuff on the dev/stage servers
 #export http_proxy=$PROXY
 #export https_proxy=$PROXY
 #export no_proxy="localhost,127.0.0.1,10.135.126.13,$DEVSTACK_IP"
 unset http_proxy https_proxy no_proxy
 
-LASTTESTED=0
-if [ -e AT-last-tested ]; then
-    LASTTESTED=`cat AT-last-tested | cut -d'v' -f2`
-fi
+#LASTTESTED=0
+#if [ -e AT-last-tested ]; then
+#    LASTTESTED=`cat AT-last-tested | cut -d'v' -f2`
+#fi
+LASTTESTED=$(get_last_tested)
 wget http://$REPOSERVER/$SNAPDIR/latest-snapshot --no-cache --no-proxy
-REPOLATEST=`cat latest-snapshot | cut -d'v' -f2`
+REPOLATEST=$(get_target_version $snapshot_version latest-snapshot)
+#REPOLATEST=`cat latest-snapshot | cut -d'v' -f2`
 rm -f latest-snapshot 2> /dev/null
 
 if [ $LASTTESTED -eq $REPOLATEST ]; then
     echo nothing new to test
+    echo v$REPOLATEST > AT-last-tested
     exit 0
 elif [ $LASTTESTED -gt $REPOLATEST ]; then
     echo LASTTESTED is greater than REPOLATEST!!
+    echo v$REPOLATEST > AT-last-tested
     exit 2
 fi
 
