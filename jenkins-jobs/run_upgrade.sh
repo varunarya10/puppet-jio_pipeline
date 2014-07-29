8ddda1addd518c6978b24f548a0833756c35f9ed
Yogeshwar Shenoy <ynshenoy@gmail.com>
2014-07-29 11:42:54 +0530

    Fix issues with correctly handling the last tested version for upgrade stage.

--- jenkins-jobs/run_upgrade.sh
+++ jenkins-jobs/run_upgrade.sh
@@ -15,13 +15,38 @@ function get_snapshot_version {
     echo "$SNAP_VER"
 }
 
+function get_last_tested {
+    local SNAP_VER=0
+    local VER1=0
+    local ARTIFACT1="lastsuccess/UPGRADE-last-tested"
+    local VER2=0
+    local ARTIFACT2="lastunsuccess/UPGRADE-last-tested"
+    if [ -e $ARTIFACT1 ]
+    then  
+        VER1=`cat $ARTIFACT1 | cut -d'v' -f2`
+    fi    
+    if [ -e $ARTIFACT2 ]
+    then  
+        VER2=`cat $ARTIFACT2 | cut -d'v' -f2`
+    fi    
+    if [ $VER1 -gt $VER2 ]
+    then
+        SNAP_VER=$VER1 
+    else
+        SNAP_VER=$VER2
+    fi
+    echo "$SNAP_VER" 
+}
+
 # Figure out the base and target versions
 BASE_VERSION=$(get_snapshot_version $base_snapshot staging-version)
 TARGET_VERSION=$(get_snapshot_version $target_snapshot NFT-last-success)
+LASTTESTED=$(get_last_tested)
 
 if [ $BASE_VERSION -eq -1 ]
 then
     echo "No base (stage) version available, nothing to test."
+    echo v$TARGET_VERSION >> UPGRADE-last-tested
     exit 0
 fi
 
@@ -29,6 +54,18 @@ if [ $TARGET_VERSION -eq -1 ]
 then
     echo "ERROR: Target version to upgrade to is not available."
     exit 1
+fi
+
+if [ $LASTTESTED -eq $TARGET_VERSION ]
+then
+    echo "Nothing new to test"
+    echo v$TARGET_VERSION > UPGRADE-last-tested
+    echo v$TARGET_VERSION > UPGRADE-last-success
+    exit 0
+elif [ $LASTTESTED -gt $TARGET_VERSION ]
+then
+    echo "LASTTESTED is greater than TARGET_VERSION"
+    exit 2
 fi
 
 unset http_proxy https_proxy no_proxy
@@ -49,3 +86,5 @@ echo "Tempest tests successful! Done"
 
 ./destroy.sh upgrade$$
 
+echo v$TARGET_VERSION > UPGRADE-last-tested
+echo v$TARGET_VERSION > UPGRADE-last-success
